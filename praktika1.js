ddd53fb0d1b054dbe9ca1c15a32ef84b734c3d52
Yulia <ivanova.yua1@yandex.ru>
2022-04-20 23:35:09 +0300

    finalFilms

--- praktika1.js
+++ praktika1.js
@@ -1,3 +1,5 @@
+
+
 const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
 const personalMovieDB ={
     count: numberOfFilms,
@@ -7,5 +9,12 @@ const personalMovieDB ={
     privat: false 
 };
 
+const lastFilm = prompt("Один из последних просмотренных фильмов?", "");
+const gradeLastFilm = prompt("На сколько оцените его?", "");
+const secondLastFilm = prompt("Один из последних просмотренных фильмов?", "");
+const secondGradeLastFilm = prompt("На сколько оцените его?", "");
 
+personalMovieDB.movies[lastFilm] = gradeLastFilm;
+personalMovieDB.movies[secondLastFilm] = secondGradeLastFilm;
 
+console.log(personalMovieDB);
